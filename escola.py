149608c62b657e3dc5e28194af8f60206237b22b
GrazihSilva <grazinha_deoliveira@hotmail.com>
2014-02-04 21:41:08 -0200

    - Tabela Curso com Chave -

--- escola.py
+++ escola.py
@@ -1,7 +1,8 @@
 class Escola:
-	def __init__(self, id, nome, email, site):
-		self.id = id
+	def __init__(self, codigo, nome, email, site, curso=None):
+		self.codigo = codigo
 		self.nome = nome
 		self.email = email
 		self.site = site
 		 
+		self.curso = curso
--- spec/escola_spec.py
+++ spec/escola_spec.py
@@ -1,12 +1,16 @@
 import unittest
 from should_dsl import should
 from escola import Escola
+from curso import Curso
 
 
 class EscolaSpec(unittest.TestCase):
 	def it_creates_a_Escola(self):
-		escola = Escola('001', 'IFF', 'iff.com.br', 'iff.edu.br')
-		escola.id |should| equal_to('001')
+		curso = Curso(002, 'alguem', 18, 2)
+		escola = Escola('001', 'IFF', 'iff.com.br', 'iff.edu.br', curso)
+		escola.codigo |should| equal_to('001')
 		escola.nome |should| equal_to('IFF')
 		escola.email |should| equal_to('iff.com.br')
 		escola.site |should| equal_to('iff.edu.br')
+
+		escola.curso |should| equal_to(curso)
