6033e32098802d0ca3f5fa97188c5c0cba0f3487
maycon7rocha <maycon7rocha@gmail.com>
2014-02-04 21:17:52 -0200

    tabela curso

---
+++ curso.py
@@ -0,0 +1,6 @@
+class Curso:
+	def __init__(self, codigo, nome, cargaHoraria, aulas):
+		self.codigo = codigo
+		self.nome = nome
+		self.cargaHoraria = cargaHoraria
+		self.aulas = aulas
---
+++ spec/curso_spec.py
@@ -0,0 +1,14 @@
+import unittest
+from should_dsl import should
+from curso import Curso
+
+class CursoSpec(unittest.TestCase):
+    
+    def it_Criar_objeto_curso(self):
+
+    	curso = Curso(002, 'alguem', 18, 2)
+
+        curso.codigo |should| equal_to(002)
+        curso.nome |should| equal_to('alguem')
+        curso.cargaHoraria |should| equal_to(18)
+        curso.aulas |should| equal_to(2)
