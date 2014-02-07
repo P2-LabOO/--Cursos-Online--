180159b7357dba546b083f275c6086fc2e075124
maycon7rocha <maycon7rocha@gmail.com>
2014-02-07 19:30:22 -0200

    tabela pagamento

---
+++ pagamento.py
@@ -0,0 +1,5 @@
+class Pagamento:
+	def __init__(self, codigo, boleto, cartaoDebito):
+		self.codigo = codigo
+		self.boleto = boleto
+		self.cartaoDebito = cartaoDebito
---
+++ spec/pagamento_spec.py
@@ -0,0 +1,13 @@
+import unittest
+from should_dsl import should
+from pagamento import Pagamento
+
+class PagamentoSpec(unittest.TestCase):
+    
+    def it_Criar_objeto_pagamento(self):
+
+    	pagamento = Pagamento(002, 1919191919, 18)
+
+        pagamento.codigo |should| equal_to(002)
+        pagamento.boleto |should| equal_to(1919191919)
+        pagamento.cartaoDebito |should| equal_to(18)
