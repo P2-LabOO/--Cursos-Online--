121d4e0cb7ccd6751e7162d93a567f1a9a11b007
GrazihSilva <grazinha_deoliveira@hotmail.com>
2014-03-04 17:15:21 -0300

    Delete escola.py

--- escola.py
+++
@@ -1,8 +0,0 @@
-class Escola:
-	def __init__(self, codigo, nome, email, site, curso=None):
-		self.codigo = codigo
-		self.nome = nome
-		self.email = email
-		self.site = site
-		 
-		self.curso = curso
